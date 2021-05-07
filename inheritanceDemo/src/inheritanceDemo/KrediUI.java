19656bfbe68d62b65d4769e1254ee273b6855e32
ibrahimcoskun <b.karinca1@gmail.com>
2021-05-07 21:38:56 +0300

    07.05.2021 inheritance

---
+++ inheritanceDemo/src/inheritanceDemo/KrediUI.java
@@ -0,0 +1,14 @@
+package inheritanceDemo;
+
+public class KrediUI {
+	public void KrediHesapla(OgretmenKrediManager ogretmenKrediManager) {
+		ogretmenKrediManager.Hesapla();
+		
+		
+	}
+	
+	public void KrediHesapla(AskerKrediManager askerKrediManager) {
+		askerKrediManager.Hesapla();
+	}
+
+}
